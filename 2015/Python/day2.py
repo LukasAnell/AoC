99ad219b7f82394de559d1dd021f9b5472db0c84
Lukas Anell <lukasmanell@gmail.com>
2024-12-15 01:11:51 -0800

    Python - 2015 Day 2

---
+++ 2015/Python/day2.py
@@ -0,0 +1,31 @@
+import operator
+from functools import reduce
+
+
+def main():
+    partOne()
+    partTwo()
+
+def partOne():
+    file = open("Inputs/2.txt", "r")
+    squareFeet = 0
+    for line in file:
+        dimensions = [int(num) for num in line.split("x")]
+        for i in range(len(dimensions)):
+            for j in range(len(dimensions)):
+                if i != j:
+                    squareFeet += dimensions[i] * dimensions[j]
+        squareFeet += sorted(dimensions)[0] * sorted(dimensions)[1]
+    print(squareFeet)
+
+def partTwo():
+    file = open("Inputs/2.txt", "r")
+    squareFeet = 0
+    for line in file:
+        dimensions = sorted([int(num) for num in line.split("x")])
+        squareFeet += 2 * (dimensions[0] + dimensions[1])
+        squareFeet += reduce(operator.mul, dimensions)
+    print(squareFeet)
+
+if __name__ == '__main__':
+    main()
