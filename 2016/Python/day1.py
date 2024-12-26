0de130210db76ac986e2086161ea3535e6b6f658
Lukas Anell <lukasmanell@gmail.com>
2024-12-26 23:21:45 +0100

    Python - 2016 Day 1

--- 2016/Python/day1.py
+++ 2016/Python/day1.py
@@ -6,11 +6,36 @@ def main():
 def partOne():
     file = open("Inputs/1.txt", "r")
     lines = [line.strip() for line in file][0].split(", ")
-    print(lines)
+    pos = 0,0
+    facing = 0,1
+    for instruction in lines:
+        direction, distance = instruction[0], instruction[1:]
+        if direction == 'L':
+            facing = facing[1], -facing[0]
+        else:
+            facing = -facing[1], facing[0]
+        pos = pos[0] + facing[0]*int(distance), pos[1] + facing[1]*int(distance)
+    print(abs(pos[0]) + abs(pos[1]))
 
 
 def partTwo():
-    pass
+    file = open("Inputs/1.txt", "r")
+    lines = [line.strip() for line in file][0].split(", ")
+    pos = 0,0
+    facing = 0,1
+    visited = set()
+    for instruction in lines:
+        direction, distance = instruction[0], instruction[1:]
+        if direction == 'L':
+            facing = facing[1], -facing[0]
+        else:
+            facing = -facing[1], facing[0]
+        for i in range(int(distance)):
+            pos = pos[0] + facing[0], pos[1] + facing[1]
+            if pos in visited:
+                print(abs(pos[0]) + abs(pos[1]))
+                return
+            visited.add(pos)
 
 
 if __name__ == '__main__':
