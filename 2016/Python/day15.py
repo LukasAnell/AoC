9f3dc7bb1127b7730bd5f4b469f699a6f49e6d38
Lukas Anell <lukasmanell@gmail.com>
2025-01-07 10:37:50 -0800

    Python - 2016 Day 15

--- 2016/Python/day15.py
+++ 2016/Python/day15.py
@@ -3,12 +3,37 @@ def main():
     partTwo()
 
 
-def partOne():
+def getDiscs():
     file = open("Inputs/15.txt", "r")
+    discs = []
+    for line in file:
+        line = line.strip().split(" ")
+        discs.append([int(line[3]), int(line[-1][:-1])])
+    return discs
+
+
+def findFirstTime(discs):
+    time = 0
+    while True:
+        success = True
+        for i in range(len(discs)):
+            if (discs[i][1] + time + i + 1) % discs[i][0] != 0:
+                success = False
+                break
+        if success:
+            break
+        time += 1
+    return time
+
+
+def partOne():
+    print(findFirstTime(getDiscs()))
 
 
 def partTwo():
-    pass
+    discs = getDiscs()
+    discs.append([11, 0])
+    print(findFirstTime(discs))
 
 
 if __name__ == "__main__":
