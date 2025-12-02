6bc8ee729832c5f407b3645cddc4d61b515167eb
LukasAnell <lukasmanell@gmail.com>
2025-12-02 12:45:13 -0500

    2025 Days 1 & 2

---
+++ 2025/Python/day1.py
@@ -0,0 +1,60 @@
+def main():
+    partOne()
+    partTwo()
+
+
+def partOne():
+    file = open("../Inputs/1.txt", "r")
+    times = 0
+    currentNum = 50
+    for line in file:
+        line = line.strip()
+        direction = line[0]
+        distance = int(line[1:])
+
+        if direction == "R":
+            currentNum = (currentNum + distance) % 100
+        else:
+            currentNum = (currentNum - distance) % 100
+
+        if currentNum == 0:
+            times += 1
+
+    print(times)
+
+
+def partTwo():
+    file = open("../Inputs/1.txt", "r")
+    times = 0
+    currentNum = 50
+    for line in file:
+        line = line.strip()
+        direction = line[0]
+        distance = int(line[1:])
+
+        if direction == "R":
+            if currentNum != 0:
+                distanceToZero = 100 - currentNum
+            else:
+                distanceToZero = 100
+
+            if distance >= distanceToZero:
+                times += 1 + (distance - distanceToZero) // 100
+
+            currentNum = (currentNum + distance) % 100
+        else:
+            if currentNum != 0:
+                distanceToZero = currentNum
+            else:
+                distanceToZero = 100
+
+            if distance >= distanceToZero:
+                times += 1 + (distance - distanceToZero) // 100
+
+            currentNum = (currentNum - distance) % 100
+
+    print(times)
+
+
+if __name__ == '__main__':
+    main()
---
+++ 2025/Python/day2.py
@@ -0,0 +1,59 @@
+def main():
+    partOne()
+    partTwo()
+
+
+def partOne():
+    line = open("../Inputs/2.txt", "r")
+    ranges = []
+    for line in line:
+        ranges += line.strip().split(",")
+    total = 0
+    for r in ranges:
+        if "-" not in r:
+            continue
+
+        lower = int(r.split("-")[0])
+        upper = int(r.split("-")[1])
+
+        for i in range(lower, upper + 1):
+            if str(i)[:len(str(i)) // 2] == str(i)[len(str(i)) // 2:]:
+                total += i
+    print(total)
+
+
+def partTwo():
+    line = open("../Inputs/2.txt", "r")
+    ranges = []
+    for line in line:
+        ranges += line.strip().split(",")
+    total = 0
+    for r in ranges:
+        if "-" not in r:
+            continue
+
+        lower = int(r.split("-")[0])
+        upper = int(r.split("-")[1])
+
+        for i in range(lower, upper + 1):
+            if isRepetition(str(i)):
+                total += i
+
+    print(total)
+
+
+def isRepetition(inputString: str):
+    length = len(inputString)
+
+    for l in range(1, length // 2 + 1):
+        if length % l != 0:
+            continue
+
+        if inputString == inputString[:l] * (length // l):
+            return True
+
+    return False
+
+
+if __name__ == '__main__':
+    main()
