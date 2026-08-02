55789c17c8f6b70deabb96ce2601ef6a9bda71bc
LukasAnell <lukasmanell@gmail.com>
2026-08-02 09:54:18 +0900

    2020 Day 02 Part 1

--- java/2020/day02/Main.java
+++ java/2020/day02/Main.java
@@ -1,7 +1,11 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
 
@@ -17,7 +21,38 @@ public class Main {
     }
 
     private static void partOne(List<String> lines) {
-        // TODO
+        int validPasswords = 0;
+
+        for (String line : lines) {
+            String[] parts = line.split(" ");
+
+            String[] range = parts[0].split("-");
+            int lowerBound = Integer.parseInt(range[0]);
+            int upperBound = Integer.parseInt(range[1]);
+
+            String targetLetter = parts[1].substring(0, 1);
+
+            String password = parts[2];
+
+            Map<Character, Integer> passwordChars = new HashMap<>();
+
+            for (char c : password.toCharArray()) {
+                if (c == targetLetter.charAt(0)) {
+                    passwordChars.put(c, passwordChars.getOrDefault(c, 0) + 1);
+                }
+            }
+
+            if (
+                passwordChars.getOrDefault(targetLetter.charAt(0), 0) >=
+                    lowerBound &&
+                passwordChars.getOrDefault(targetLetter.charAt(0), 0) <=
+                    upperBound
+            ) {
+                validPasswords++;
+            }
+        }
+
+        System.out.println("Part One: " + validPasswords);
     }
 
     private static void partTwo(List<String> lines) {
